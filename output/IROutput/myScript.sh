56df46b6286b53d402199e09f4743382985abd00
Adam Morris <amm4@cs.brown.edu>
2014-08-17 15:29:04 -0400

    Finished StealPunish4 (w/ init q values lowered)

--- output/IROutput/myScript.sh
+++ output/IROutput/myScript.sh
@@ -3,4 +3,4 @@
 #PBS -e /dev/null
 #PBS -o /dev/null
 
-/gpfs/main/home/amm4/java/jdk1.8.0_05/bin/java -jar ShareReciprocate.jar "/gpfs/main/home/amm4/git/generalResearch/IROutput/ShareReciprocate2" $SGE_TASK_ID
+/gpfs/main/home/amm4/java/jdk1.8.0_05/bin/java -jar StealPunish.jar "/gpfs/main/home/amm4/git/generalResearch/output/IROutput/StealPunish4" $SGE_TASK_ID
